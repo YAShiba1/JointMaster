dcb9a4850dda140300ee3bb70a7a5d52edd553a6
YAShiba1 <108460620+YAShiba1@users.noreply.github.com>
2024-05-04 13:32:12 +0300

    second

--- Assets/Scripts/Swing.cs
+++ Assets/Scripts/Swing.cs
@@ -9,7 +9,7 @@ public class Swing : MonoBehaviour
     private HingeJoint _hingeJoint;
     private JointMotor _jointMotor;
 
-    private readonly KeyCode KeySpace = KeyCode.Space;
+    private readonly KeyCode ShotKey = KeyCode.Space;
 
     private void Start()
     {
@@ -24,7 +24,7 @@ public class Swing : MonoBehaviour
 
     private void Ride()
     {
-        if (Input.GetKeyDown(KeySpace))
+        if (Input.GetKeyDown(ShotKey))
         {
             _jointMotor.targetVelocity = _swingForce;
             _jointMotor.force = _force;
